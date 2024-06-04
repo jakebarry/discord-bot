9e9f385f09f3c59cb0d4f817e85aafcf6381825b
jakebarry <168513840+jakebarry@users.noreply.github.com>
2024-06-04 16:38:15 +1000

    Adding database functionality.

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ load_dotenv(override=True)
 
 BOT_TOKEN: Final[str] = os.getenv("BOT_TOKEN")
 CHANNEL_ID: Final[int] = int(os.getenv("CHANNEL_ID"))
+MAX_TIMEOUT: Final[int] = 60
 
 intents: Intents = Intents.default()
 intents.message_content = True
@@ -32,9 +33,9 @@ accepted_websites = ["recipetineats"]
 
 # Initialise meal class
 class Meal:
-    def __init__(self, name, type, ingredients, instructions):
+    def __init__(self, name, category, ingredients, instructions):
         self.name = name
-        self.type = type
+        self.category = category
         self.ingredients = ingredients
         self.instructions = instructions
 
@@ -77,7 +78,7 @@ async def addmeal(ctx):
         return msg.author == ctx.author and msg.channel == ctx.channel
     
     try:
-        user_link_choice = await bot.wait_for('message', check=check, timeout=60)
+        user_link_choice = await bot.wait_for('message', check=check, timeout=MAX_TIMEOUT)
     except asyncio.TimeoutError:
         await ctx.send("You took too long to respond.")
         return
@@ -94,63 +95,158 @@ async def addmeal(ctx):
         return
 
     # Check if website is valid
-    if valid:
-        await ctx.send("That URL is valid, thank-you.")
-    else:
-        await ctx.send("Invalid URL. Try again.")
+    # if valid:
+    #     await ctx.send("That URL is valid, thank-you.")
+    # else:
+    #     await ctx.send("Invalid URL. Try again.")
 
-    if recipe_scraper(ctx, link_choice):
-        await ctx.send("Recipe has been added.")
+    if not valid:
+        await ctx.send("Invalid URL. Try again.")
+        
+    await ctx.send("What category do you wish to place this recipe into?"
+                "\n1. Vegetarian\n2. Low-Carb\n3. High-Protein\n")
+    # Define check function to filter messages from the original author and the same channel
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+    
+    try:
+        user_meal_choice = await bot.wait_for('message', check=check, timeout=60)
+    except asyncio.TimeoutError:
+        await ctx.send("You took too long to respond.")
+        return
+    
+    meal_choice = user_meal_choice.content.lower()
+
+    if meal_choice in ['1', 'vegetarian']:
+        category = "Vegetarian"
+    elif meal_choice in ['2', 'low-carb', 'low', 'carb']:
+        category = "Low-Carb"
+    elif meal_choice in ['3', 'high-protein', 'high', 'protein']:
+        category = "High-Protein"
+
+    # Need to loop to allow multiple choices
     else:
+        await ctx.send("Invalid choice. Please select a valid option.")
+        return
+    
+
+
+    recipe = recipe_scraper(ctx, link_choice, category) 
+
+    if recipe == 0:
         await ctx.send("Invalid URL. Make sure the URL directs toward a recipe with ingredients and instructions.")
+    elif recipe == 1:
+        await ctx.send("Recipe has been added.")
+    elif recipe == 2:
+        await ctx.send("This recipe already exists in the database.")
+
+
+        
+
+    # if recipe_scraper(ctx, link_choice):
+    #     await ctx.send("Recipe has been added.")
+    # else:
+    #     await ctx.send("Invalid URL. Make sure the URL directs toward a recipe with ingredients and instructions.")
 
     
 
 
 
-
-def recipe_scraper(ctx, URL):
+# Returns {0: ,1: , 2: }
+def recipe_scraper(ctx, URL, category):
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
 
-    results = soup.find(id="wprm-recipe-container-25094")
+    results = soup.find(class_="wprm-recipe-container")
+
 
     try:
         recipe_name = results.find("h2", class_="wprm-recipe-name wprm-block-text-bold")
         ingredient_elements = results.find_all("li", class_="wprm-recipe-ingredient")
         instruction_elements = results.find_all("li", class_="wprm-recipe-instruction")
     except AttributeError as e:
-        print(e)
-        return False
+        # print(e)
+        # print("Invalid URL")
+        return 0
     
     # ingredient_elements = results.find_all("span", class_="wprm-recipe-ingredient-name")
 
 
-    print(f"\n               {recipe_name.text}               ")
-    print("\n**Ingredients:**\n")
+    # print(f"\n               {recipe_name.text}               ")
+    # print("\n**Ingredients:**\n")
+
+    ingredients = []
+    instructions = []
+
     for ingredient_element in ingredient_elements:
+        ingredient = {}
         amount_element = ingredient_element.find("span", class_="wprm-recipe-ingredient-amount")
         unit_element = ingredient_element.find("span", class_="wprm-recipe-ingredient-unit")
         name_element = ingredient_element.find("span", class_="wprm-recipe-ingredient-name")
         if amount_element != None:
-            print(f"{amount_element.text} ", end="")
+            ingredient["amount"] = amount_element.text
+            # print(f"{amount_element.text} ", end="")
         if unit_element != None:
-            print(f"{unit_element.text} ", end="")
+            ingredient["unit"] = unit_element.text
+            # print(f"{unit_element.text} ", end="")
         if name_element != None:
-            print(name_element.text)
-    print()
+            ingredient["name"] = name_element.text
+            # print(name_element.text)
 
-    print("\n**Instructions:**\n")
+        # ingredient = {
+        #     "amount": amount_element,
+        #     "unit": unit_element,
+        #     "name": name_element
+        # }
+
+        ingredients.append(ingredient)
+    
+    # print(ingredients)
+
+
+    # print("\n**Instructions:**\n")
     index = 1
     for instruction_element in instruction_elements:
         text_element = instruction_element.find("div", class_="wprm-recipe-instruction-text")
         if text_element != None:
-            print(f"{index}: {text_element.text}")
+            instruction = {
+                "step": index,
+                "description": text_element.text
+            }
+
+            # print(f"{index}: {text_element.text}")
             index += 1
+        instructions.append(instruction)
+    
+    # print(instructions)
         
-    print()
-    return True
+
+
+    recipe = {
+        "name": recipe_name.text,
+        "category": category,
+        "ingredients": ingredients,
+        "instructions": instructions
+    }
+
+    # print(recipe)
+
+
+    # Check if the recipe already exists in the database
+    db_session = get_db_session()
+
+    # print(db_session.find_one({"name": recipe_name.text}))
+    # print(db_session.count_documents({"name": recipe_name.text}))
+
+    if db_session.count_documents({"name": recipe_name.text}) < 1:
+        # print("\n** TESTING **")
+        db_session.insert_one(recipe)
+        return 1
+    # else:
+        # ctx.send("This recipe is already in the database.")
+
+    return 2
 
 
 
@@ -165,9 +261,9 @@ async def websites(ctx):
 @bot.command(brief='See all available meals in database')
 async def seemeals(ctx):
     db_session = get_db_session()
-    meal_list = db_session.find({}, {"Meal": 1})
+    meal_list = db_session.find({}, {"name": 1})
     for meal in meal_list:
-        await ctx.send(meal['Meal'])
+        await ctx.send(meal['name'])
 
 
 
@@ -192,6 +288,28 @@ async def planmeal(ctx):
     except asyncio.TimeoutError:
         await ctx.send("You took too long to respond.")
         return
+    
+
+    meal_choice = user_meal_choice.content.lower()
+
+    if meal_choice in ['1', 'vegetarian']:
+        meals = db_session.find({'category': 'Vegetarian'})
+    elif meal_choice in ['2', 'low-carb', 'low', 'carb']:
+        meals = db_session.find({'category': 'Low-Carb'})
+    elif meal_choice in ['3', 'high-protein', 'high', 'protein']:
+        meals = db_session.find({'category': 'High-Protein'})
+    else:
+        await ctx.send("Invalid choice. Please select a valid option.")
+        return
+    
+
+    meals_list = [meal for meal in meals]
+    
+    if meals_list:
+        selected_meal = random.choice(meals_list)
+        await ctx.send(f"Here's your meal suggestion based on your choice:\n{selected_meal['Meal']}")
+    else:
+        await ctx.send("No meals found for the selected category.")
 
     # meal = None
     # if user_meal_choice.content.lower() in ['1', 'vegetarian']:
@@ -209,27 +327,6 @@ async def planmeal(ctx):
     # if meal:
     #     await ctx.send(f"Here's your meal suggestion based on your choice:\n{meal}")
 
-
-    meal_choice = user_meal_choice.content.lower()
-
-    if meal_choice in ['1', 'vegetarian']:
-        meals = db_session.find({'Type': 'Vegetarian'})
-    elif meal_choice in ['2', 'low-carb', 'low', 'carb']:
-        meals = db_session.find({'Type': 'Low-Carb'})
-    elif meal_choice in ['3', 'high-protein', 'high', 'protein']:
-        meals = db_session.find({'Type': 'High-Protein'})
-    else:
-        await ctx.send("Invalid choice. Please select a valid option.")
-        return
-    
-
-    meals_list = [meal for meal in meals]
-    
-    if meals_list:
-        selected_meal = random.choice(meals_list)
-        await ctx.send(f"Here's your meal suggestion based on your choice:\n{selected_meal['Meal']}")
-    else:
-        await ctx.send("No meals found for the selected type.")
 
 
 
